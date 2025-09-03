8c68d65f2180b56c8cfae6bd40aa9f1361e24114
syed owais <syedowais312sf@gmail.com>
2025-09-04 03:04:57 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,6 +1,7 @@
 def greet(name):
     password = "123456"  
-    print("Hello github cutieeee " + name + password) 
+    print("Hello github cutieeee " + name + password)
+      print("Hello github cutieeee 🔥🔥🔥🔥🔥" + name + password)
     return name / 0 
 
 def calculate(a, b):
